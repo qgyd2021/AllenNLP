316e86d9af85e83cc0e0e3de6818f1d904479a0e
晴耕雨读 <qgyd2021@gmail.com>
2023-07-25 18:04:47 +0800

    [update]conll2012

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+
+system_version="centos"
+python_version=3.8.10
+
+verbose=true;
+stage=-1
+stop_stage=3
+
+
+# parse options
+while true; do
+  [ -z "${1:-}" ] && break;  # break if there are no arguments
+  case "$1" in
+    --*) name=$(echo "$1" | sed s/^--// | sed s/-/_/g);
+      eval '[ -z "${'"$name"'+xxx}" ]' && echo "$0: invalid option $1" 1>&2 && exit 1;
+      old_value="(eval echo \\$$name)";
+      if [ "${old_value}" == "true" ] || [ "${old_value}" == "false" ]; then
+        was_bool=true;
+      else
+        was_bool=false;
+      fi
+
+      # Set the variable to the right value-- the escaped quotes make it work if
+      # the option had spaces, like --cmd "queue.pl -sync y"
+      eval "${name}=\"$2\"";
+
+      # Check that Boolean-valued arguments are really Boolean.
+      if $was_bool && [[ "$2" != "true" && "$2" != "false" ]]; then
+        echo "$0: expected \"true\" or \"false\": $1 $2" 1>&2
+        exit 1;
+      fi
+      shift 2;
+      ;;
+
+    *) break;
+  esac
+done
+
+
+work_dir="$(pwd)"
+
+
+if [ $system_version == "centos" ]; then
+  cd "${work_dir}" || exit 1;
+
+  if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    $verbose && echo "stage 0: install python"
+    cd "${work_dir}" || exit 1;
+
+    sh ./script/install_python.sh --python_version "${python_version}" --system_version "${system_version}"
+  fi
+
+  if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    $verbose && echo "stage 1: create virtualenv"
+    /usr/local/python-${python_version}/bin/pip3 install virtualenv
+    mkdir -p /data/local/bin
+    cd /data/local/bin || exit 1;
+    # source /data/local/bin/AllenNLP/bin/activate
+    /usr/local/python-${python_version}/bin/virtualenv AllenNLP
+
+  fi
+
+fi
