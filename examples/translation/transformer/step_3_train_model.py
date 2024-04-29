3cfcb50c71bee4f2e273ed8070305e4622c9383a
Honey田兴 <honeyTian@nxcloud.com>
2024-04-29 13:39:21 +0800

    update

---
+++ examples/translation/transformer/step_3_train_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+http://www.realworldnlpbook.com/blog/building-seq2seq-machine-translation-models-using-allennlp.html
+"""
+from allennlp_models.generation.models.simple_seq2seq import SimpleSeq2Seq
+
+
+if __name__ == "__main__":
+    pass
