7c5648dcdfc0b5fcf695c200d8c1bfbbc3e27704
Honey田兴 <honeyTian@nxcloud.com>
2024-04-29 13:43:39 +0800

    update

--- examples/text_classification/language_identification/step_4_evaluation.py
+++ examples/text_classification/language_identification/step_4_evaluation.py
@@ -61,46 +61,45 @@ def main():
 
     result = list()
 
-    for filename in [args.train_subset, args.valid_subset]:
-        total_count = 0
-        correct_count = 0
-        with tqdm() as progress_bar:
-            with open(filename, "r", encoding="utf-8") as f:
-                subset = Path(filename).stem
-                for row in f:
-                    row = json.loads(row)
-                    text = row["text"]
-                    ground_true = row["label"]
-                    split = row["split"]
+    total_count = 0
+    correct_count = 0
+    with tqdm() as progress_bar:
+        with open(args.valid_subset, "r", encoding="utf-8") as f:
+            subset = Path(args.valid_subset).stem
+            for row in f:
+                row = json.loads(row)
+                text = row["text"]
+                ground_true = row["label"]
+                split = row["split"]
 
-                    json_dict = {
-                        "sentence": text
-                    }
+                json_dict = {
+                    "sentence": text
+                }
 
-                    outputs = predictor.predict_json(
-                        json_dict
-                    )
-                    predict = outputs["label"]
-                    probs = outputs["probs"]
-                    prob = round(max(probs), 4)
-                    correct = 1 if ground_true == predict else 0
+                outputs = predictor.predict_json(
+                    json_dict
+                )
+                predict = outputs["label"]
+                probs = outputs["probs"]
+                prob = round(max(probs), 4)
+                correct = 1 if ground_true == predict else 0
 
-                    result.append({
-                        "subset": subset,
-                        "split": split,
-                        "text": text,
-                        "ground_true": ground_true,
-                        "predict": predict,
-                        "prob": prob,
-                        "correct": correct
-                    })
-                    total_count += 1
-                    correct_count += correct
-                    accuracy = correct_count / total_count
-                    accuracy = round(accuracy, 4)
+                result.append({
+                    "subset": subset,
+                    "split": split,
+                    "text": text,
+                    "ground_true": ground_true,
+                    "predict": predict,
+                    "prob": prob,
+                    "correct": correct
+                })
+                total_count += 1
+                correct_count += correct
+                accuracy = correct_count / total_count
+                accuracy = round(accuracy, 4)
 
-                    progress_bar.update(1)
-                    progress_bar.set_postfix({"accuracy": accuracy, "subset": subset})
+                progress_bar.update(1)
+                progress_bar.set_postfix({"accuracy": accuracy, "subset": subset})
 
     result = pd.DataFrame(result)
     result.to_excel(args.evaluation_output_file, index=False, encoding="utf_8_sig", engine="xlsxwriter")
